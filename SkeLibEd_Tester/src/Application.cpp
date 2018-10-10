0db56bff4585684776d44d402b47f59dcb55aeef
Boyan Yotov <boyan.yotov@abv.bg>
2018-10-10 21:06:22 +0100

    First implementation of grapher and development of functionality to create test data files with plotters

--- SkeLibEd_Tester/src/Application.cpp
+++ SkeLibEd_Tester/src/Application.cpp
@@ -36,18 +36,20 @@ int main()
 	int elemental_arg = 2;
 
 	// Thread test argument and Block test argument
-	testArgument ti = { 2, 128, 0, 2 };
-	testArgument bi = { 2, 128, 0, 2 };
-	int tests = 10;
-	TesterMap tmap(tests, ti, bi);
+	testArgument ti = { 2, 32, 0, 2 };
+	testArgument bi = { 2, 32, 0, 2 };
+	int tests = 100;
+
+    // Grapher declaration
+	TesterMap tmap("colatz 271", tests, ti, bi);
 	tmap.test(elemental, in, out, elemental_arg);
 
 
 	// Check output
 	//for (int i = 0; i < nItems; i++)
-		std::cout << "Item [" << nItems - 1 << "] = " << out[nItems-1] << std::endl;
+	//	std::cout << "Item [" << nItems - 1 << "] = " << out[nItems-1] << std::endl;
 
 	//normal_main();
 
-	//std::cin.get();
+	std::cin.get();
 }
